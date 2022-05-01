19fcf1a78cdbf0e5f8ec084b63d23b7c2cacaafe
SeungWoo Jung <anes53027@gmail.com>
2022-05-01 13:07:36 +0900

    [Silver V] Title: 소인수분해, Time: 144 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.java
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int prime[];
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-
-        prime = new int[N+1];
 
         for(int i = 2; i <= N; i++){
             if(N % i == 0){
