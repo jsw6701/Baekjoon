678a95a24ce5208387973c092d28c245d78bfac4
SeungWoo Jung <anes53027@gmail.com>
2022-05-07 23:44:00 +0900

    [Bronze II] Title: 음계, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        if(s.equals("1 2 3 4 5 6 7 8")){
+            bw.write("ascending");
+        }
+        else if(s.equals("8 7 6 5 4 3 2 1")){
+            bw.write("descending");
+        }
+        else{
+            bw.write("mixed");
+        }
+        bw.close();
+    }
+}
