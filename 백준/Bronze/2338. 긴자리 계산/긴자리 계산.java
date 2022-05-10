91e86886fa313984234086447df156cac7d2e60a
SeungWoo Jung <anes53027@gmail.com>
2022-05-10 09:36:33 +0900

    [Bronze V] Title: 긴자리 계산, Time: 128 ms, Memory: 13260 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger big1 = new BigInteger(sc.next());
+        BigInteger big2 = new BigInteger(sc.next());
+
+        System.out.println(big1.add(big2));
+        System.out.println(big1.subtract(big2));
+        System.out.println(big1.multiply(big2));
+    }
+}
