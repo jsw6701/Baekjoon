efd8fbc6772027aacabb146b48c348ccaafc0ead
SeungWoo Jung <anes53027@gmail.com>
2022-08-12 16:16:42 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long[] arr = new long[101];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for (int i = 3; i < 101; i++) {
+            arr[i] = arr[i-3] + arr[i-2];
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(T-->0){
+            int N = Integer.parseInt(br.readLine());
+            sb.append(arr[N]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
