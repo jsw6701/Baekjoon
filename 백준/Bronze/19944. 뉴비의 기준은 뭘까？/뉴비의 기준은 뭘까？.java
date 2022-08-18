a2f8867a2a40d7c001abac459e8b0931cc69e844
SeungWoo Jung <anes53027@gmail.com>
2022-08-19 00:09:46 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(M <= 2){
+            System.out.println("NEWBIE!");
+        }
+        else if(N - M >= 0){
+            System.out.println("OLDBIE!");
+        }
+        else{
+            System.out.println("TLE!");
+        }
+    }
+}
