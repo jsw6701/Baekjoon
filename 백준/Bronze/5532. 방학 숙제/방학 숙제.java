4cbc7665ad6d44f7c4926f8dae4cf286bb8482e3
SeungWoo Jung <anes53027@gmail.com>
2022-08-19 00:42:06 +0900

    [Bronze IV] Title: 방학 숙제, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int L = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int korean = A / C;
+        int math = B / D;
+        
+        
+        if(A % C > 0){
+            korean++;
+        }
+        if(B % D > 0){
+            math++;
+        }
+
+        System.out.println(L - Math.max(korean, math));
+    }
+}
