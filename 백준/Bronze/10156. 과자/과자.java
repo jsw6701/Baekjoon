054497a01fa4e1c6c2e5bfd9570ace3f53bb4c07
SeungWoo Jung <anes53027@gmail.com>
2022-09-02 13:29:25 +0900

    [Bronze IV] Title: 과자, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int total = K * N;
+
+        if(total >= M){
+            System.out.println(total - M);
+        }
+        else{
+            System.out.println(0);
+        }
+
+    }
+}
