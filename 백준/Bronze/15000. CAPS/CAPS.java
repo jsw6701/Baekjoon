e0ae353ab5b65ee44632f22f2a8e2a1ea7fbe1d8
SeungWoo Jung <anes53027@gmail.com>
2022-09-18 19:01:31 +0900

    [Bronze IV] Title: CAPS, Time: 220 ms, Memory: 27108 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 32;
+            sb.append((char)c);
+        }
+        System.out.println(sb);
+    }
+}
