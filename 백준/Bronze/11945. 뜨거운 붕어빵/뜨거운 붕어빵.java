dcc32a5dd1b70e1c9b9774c036b8d159189e8a19
SeungWoo Jung <anes53027@gmail.com>
2022-09-20 22:37:41 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 76 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = s.charAt(j)-48;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = M-1; j >= 0; j--) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
