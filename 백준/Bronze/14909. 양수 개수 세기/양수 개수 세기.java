059ff6889319f9993e2b7632963b1f2e5ba244ba
SeungWoo Jung <anes53027@gmail.com>
2022-10-12 17:38:45 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 428 ms, Memory: 102172 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = 0;
+        while(st.hasMoreTokens()){
+            if(Integer.parseInt(st.nextToken()) > 0){
+                N++;
+            }
+        }
+        System.out.println(N);
+    }
+}
